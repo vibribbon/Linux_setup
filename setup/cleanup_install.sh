2d9d5ae039c60e20c0fa92d053829869d8d49ff9
vibribbon <109892129+vibribbon@users.noreply.github.com>
2022-08-30 21:20:29 +0100

    Create cleanup_install.sh

---
+++ setup/cleanup_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Quick script to cleanup stuff in openbox
+mkdir /home/vibri/files
+rmdir /home/vibri/Documents
+rmdir /home/vibri/Videos
+rmdir /home/vibri/Public
+rmdir /home/vibri/Pictures
+rmdir /home/vibri/Templates
+rmdir /home/vibri/Music
+rmdir /home/vibri/Desktop
+rn /home/vibri/Downloads /home/vibri/downloads
