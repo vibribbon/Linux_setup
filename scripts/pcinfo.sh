1d5d8374c5e481045ae4b17eb2f04e0d5132740c
vibribbon <109892129+vibribbon@users.noreply.github.com>
2022-08-31 12:59:56 +0100

    Create pcinfo.sh

---
+++ scripts/pcinfo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+printf "\n"
+printf "   %s\n" "IP ADDR: $(ip address)"
+printf "   %s\n" "USER: $(echo $USER)"
+printf "   %s\n" "DATE: $(date)"
+printf "   %s\n" "UPTIME: $(uptime -p)"
+printf "   %s\n" "HOSTNAME: $(hostname -f)"
+printf "   %s\n" "CPU: $(awk -F: '/model name/{print $2}' | head -1)"
+printf "   %s\n" "KERNEL: $(uname -rms)"
+printf "   %s\n" "PACKAGES: $(dpkg --get-selections | wc -l)"
+printf "   %s\n" "RESOLUTION: $(xrandr | awk '/\*/{printf $1" "}')"
+printf "   %s\n" "MEMORY: $(free -m -h | awk '/Mem/{print $3"/"$2}')"
+printf "\n"
