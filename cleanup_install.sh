551f8125962decfb85e8a38e48f13c6e2b4f1602
vibribbon <109892129+vibribbon@users.noreply.github.com>
2022-10-23 17:50:43 +0100

    Add files via upload

---
+++ cleanup_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Quick script to cleanup stuff in openbox
+mkdir /home/vibri/files
+mkdir /home/vibri/downloads
+
+rmdir /home/vibri/Documents
+rmdir /home/vibri/Videos
+rmdir /home/vibri/Public
+rmdir /home/vibri/Pictures
+rmdir /home/vibri/Templates
+rmdir /home/vibri/Music
+rmdir /home/vibri/Downloads 
+
